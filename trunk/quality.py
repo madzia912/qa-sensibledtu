cbc04af3eed215c546d0e04b0de789ee9070b93a
madzia912@gmail.com <madzia912@gmail.com@ccf55bd4-9b74-be2f-1e61-002a55cdb968>
2014-03-02 19:41:34 +0000

    Updated plots to bar charts.

--- trunk/quality.py
+++ trunk/quality.py
@@ -8,33 +8,36 @@ Created on Wed Feb 12 14:55:00 2014
 import pandas
 from datetime import datetime
 from datetime import timedelta
+import csv
 from matplotlib import pyplot as plt
 from matplotlib.dates import date2num
+from matplotlib.dates import DateFormatter
 import numpy
 import pylab as pl
-import collections
-import csv
 
-def printoutSingleVal(name, value):
-    for row in data[name]:
-        print row
-    
-def printoutSingleSum(name, value):
-    return sum(data[name]==value)
-    
-def printoutSingleReport(name, value):
-    return sum(data[name]==value)
-    
-def printoutReport(val):
-    print "uuid" + " - " + str(printoutSingleSum("uuid", val))
-    print "name" + " - " + str(printoutSingleSum("name", str(val)))
-    print "timestamp" + " - " + str(printoutSingleSum("timestamp", val))
-    print "bt_mac" + " - " + str(printoutSingleSum("bt_mac", str(val)))
-    print "class" + " - " + str(printoutSingleSum("class", val))
-    print "user" + " - " + str(printoutSingleSum("user", val))
-    print "rssi" + " - " + str(printoutSingleSum("rssi", val))
-    print "sensible_token" + " - " + str(printoutSingleSum("sensible_token", val))
-    print "device_id" + " - " + str(printoutSingleSum("device_id", val))
+DATE_FMT = "%d-%m-%y"
+YLIM = [0, 1.1]
+
+#def printoutSingleVal(name, value):
+#    for row in data[name]:
+#        print row
+#    
+#def printoutSingleSum(name, value):
+#    return sum(data[name]==value)
+#    
+#def printoutSingleReport(name, value):
+#    return sum(data[name]==value)
+#    
+#def printoutReport(val):
+#    print "uuid" + " - " + str(printoutSingleSum("uuid", val))
+#    print "name" + " - " + str(printoutSingleSum("name", str(val)))
+#    print "timestamp" + " - " + str(printoutSingleSum("timestamp", val))
+#    print "bt_mac" + " - " + str(printoutSingleSum("bt_mac", str(val)))
+#    print "class" + " - " + str(printoutSingleSum("class", val))
+#    print "user" + " - " + str(printoutSingleSum("user", val))
+#    print "rssi" + " - " + str(printoutSingleSum("rssi", val))
+#    print "sensible_token" + " - " + str(printoutSingleSum("sensible_token", val))
+#    print "device_id" + " - " + str(printoutSingleSum("device_id", val))
     
 def getUserData(user_id):
     return data[data["user"] == user_id]
@@ -59,9 +62,7 @@ def getHourData(user_id):
         tab.append((datetime.fromtimestamp(i), " "))
     return tab
     
-
-    
-def getQualityHour2(user_id):
+def getQualityHour(user_id, time):
     single_user_data = getUserData(user_id)
     single_user_timestamp = getTimestamp(single_user_data)
     first_hour = single_user_timestamp[0]
@@ -84,45 +85,16 @@ def getQualityHour2(user_id):
                     k = k + 1
             else:
                 break
-        tempdate = datetime(first_hour_dt.year, first_hour_dt.month, first_hour_dt.day, first_hour_dt.hour, 0, 0)
-                
-        counting_list.append((tempdate, float((k + 1) / 12.0)))
+        grade = float((k + 1) / 12.0)
+        if grade > 1.0:
+            grade = 1.0
+           
+        tempDate = datetime(first_hour_dt.year, first_hour_dt.month, first_hour_dt.day, first_hour_dt.hour)
+        counting_list.append((tempDate, grade))
         m = m + 1
     return counting_list
 
-def getQualityDay(user_id):
-    single_user_data = getUserData(user_id)
-    single_user_timestamp = getTimestamp(single_user_data)
-    first_hour = single_user_timestamp[0]
-    first_hour_dt = datetime.fromtimestamp(first_hour)
-    last_hour = single_user_timestamp[-1]
-    last_hour_dt = datetime.fromtimestamp(last_hour)
-    
-    i = 0
-    resDict = {}
-    while first_hour_dt < last_hour_dt and i < len(single_user_timestamp): 
-        temp_timestamp = datetime.fromtimestamp(single_user_timestamp[i])
-        if temp_timestamp >= first_hour_dt and temp_timestamp < first_hour_dt + timedelta(minutes = 5):
-            if resDict.has_key(first_hour_dt):
-                resDict[first_hour_dt] = resDict[first_hour_dt] + 1
-                i = i + 1
-            else:
-                resDict[first_hour_dt] = 1
-                i = i + 1
-        elif temp_timestamp < first_hour_dt:
-            i = i + 1
-        elif temp_timestamp >= first_hour_dt + timedelta(minutes = 5):
-            first_hour_dt = first_hour_dt + timedelta(minutes = 5) 
-            if not resDict.has_key(first_hour_dt):
-                resDict[first_hour_dt] = 0
-        else:
-            i = i + 1
-            first_hour_dt = first_hour_dt + timedelta(minutes = 5)
-        #tempdate = datetime(first_hour_dt.year, first_hour_dt.month, first_hour_dt.day, first_hour_dt.hour, 0, 0)
-        #counting_list.append((tempdate, float((k + 1) / 12.0)))                
-    return collections.OrderedDict(sorted(resDict.items()))
-    
-def getQualityDay1(user_id):
+def getQualityDay(user_id, time):
     single_user_data = getUserData(user_id)
     single_user_timestamp = getTimestamp(single_user_data)
     first_hour = single_user_timestamp[0]
@@ -145,81 +117,112 @@ def getQualityDay1(user_id):
             else:
                 break
             
-        tempdate = datetime(first_hour_dt.year, first_hour_dt.month, first_hour_dt.day, 12, 0, 0)
-        value = (k + 1) / 288.0
-        #print "K" + str(k) + "val" + str(value)
-        counting_list.append((tempdate, value))
+        tempDate = datetime(first_hour_dt.year, first_hour_dt.month, first_hour_dt.day, 0)
+        
+        grade = (k + 1) / 288.0
+        if grade > 1.0:
+            grade = 1.0
+        counting_list.append((tempDate, grade))
         m = m + 1
     return counting_list  
    
-def plotAllData(user_data, user_id):
-    x = [date2num(date) for date in user_data.keys()]
-    y = [value for value in user_data.values()]
-    yy = numpy.zeros(len(user_data))
-    
-    pl.figure()
-    pl.subplot(111)
-    pl.plot_date(x,y,'r')
-    pl.title('Quality of all data, ' + user_id)
-    pl.xticks(rotation=30)
-    pl.plot(x, yy, 'g')
-    plt.show()
-    plt.savefig('Diagrams/all_' + user_id + '.png', bbox_inches='tight')
-    plt.close()
-    
-def plotMonthData(user_data, user_id):
-    #user_data = user_data[-37:-7] # take last 30 elements
-    x = [date2num(date) for date in user_data.keys()]
-    y = [value for value in user_data.values()]
-    yy = numpy.ones(len(user_data))
-    
-    pl.figure()
-    pl.subplot(111)
-    pl.plot_date(x,y,'r-o')
-    pl.title('Quality of data last month, ' + user_id)
-    pl.xticks(rotation=30)
-    pl.plot(x, yy, 'g')
-    plt.show()
-    plt.savefig('Diagrams/month_' + user_id + '.png', bbox_inches='tight')
-    plt.close()
-    
-def plotWeekData(user_data, user_id):
-    #user_data = user_data[-7:] # take last 7 elements
+def commonPlotting(user_data, user_id, prefix, xlim=None):
     x = [date2num(date) for (date, value) in user_data]
     y = [value for (date, value) in user_data]
-    yy = numpy.ones(len(user_data))
-    
+    colors = getBarColour(user_data)
     pl.figure()
     pl.subplot(111)
-    pl.plot_date(x,y,'r-o')
-    pl.title('Quality of data last week, ' + user_id)
+    pl.bar(x,y,color=colors)
+    pl.title('Quality of all data, ' + user_id)
     pl.xticks(rotation=30)
-    pl.plot(x, yy, 'g')
+    ax = pl.gca()
+    ax.xaxis.set_major_formatter(DateFormatter(DATE_FMT))
+    pl.xlim(xlim)
+    pl.ylim([0, 1.1])
     plt.show()
-    plt.savefig('Diagrams/week_' + user_id + '.png', bbox_inches='tight') 
+    plt.savefig('pictures/' + prefix + user_id + '.png', bbox_inches='tight')
     plt.close()
+    
+def getBarColour(data):
+    colors = []
+    for value in data:
+        if value == 1.0:
+            colors.append('g')
+        else:
+            colors.append('r')
+    return colors
+    
+def plotAllData(user_data, user_id):
+    commonPlotting(user_data, user_id, 'all_')
+    
+def plotMonthData(user_data, user_id):
+    [user_data, xlim] = getMonthData(user_data)
+    commonPlotting(user_data, user_id, 'month_', xlim)
+    
+def plotWeekData(user_data, user_id):
+    [user_data, xlim] = getWeekData(user_data)
+    commonPlotting(user_data, user_id, 'week_', xlim)
+    
+def plotRandomDays(user_data, user_id):
+    for i in xrange(0, 3):
+        [user_data1, xlim] = getDayData(user_data)
+        x = [date2num(date) for (date, value) in user_data1]
+        y = [value for (date, value) in user_data1]
+        
+        colors = getBarColour(y)
+        pl.figure()
+        pl.subplot(111)
+        pl.bar(x,y,width=0.035, color=colors)
+        pl.xlim(xlim)
+        pl.title('Quality of data per hour, ' + user_id + " " + user_data1[0][0].strftime(DATE_FMT))
+        pl.xticks(rotation=30)
+        pl.ylim([0, 1.1])
+        ax = pl.gca()
+        ax.xaxis.set_major_formatter(DateFormatter("%H:%M")) 
+        plt.show()
+        plt.savefig('pictures/' + 'day_' + user_id + '_' + str(i) + '.png', bbox_inches='tight')
+        plt.close()
+    
+def getDaysList(user_data):
+    day_list = []
+    temp_scan = user_data[0]
+    for scan in user_data:
+        if( not scan[0].year == temp_scan[0].year or scan[0].month == temp_scan[0].month or scan[0].day == temp_scan[0].day):
+            day_list.append(scan)
+            temp_scan = scan
+    return day_list
+        
+def getDayData(user_data):
+    #print "day", len(getDaysList(user_data))
+    r = randint(0, len(getDaysList(user_data)))
+    #print r
+    tempScan = user_data[r]
+    scan_list = []
+    for scan in user_data:
+        if scan[0].year == tempScan[0].year and scan[0].month == tempScan[0].month and scan[0].day == tempScan[0].day:           
+            scan_list.append(scan)
+            
+    begin_date = datetime(tempScan[0].year, tempScan[0].month, tempScan[0].day, 0)
+    end_date = datetime(tempScan[0].year, tempScan[0].month, tempScan[0].day, 0) + timedelta(days = 1)
+    return [scan_list, [begin_date, end_date]]
     
 def getWeekData(data):
     end_date = data[-1][0]
     begin_date = end_date - timedelta(days = 7)
-#    print end_date
-#    print begin_date
     res = []
     for (key, val) in data:
         if key <= end_date and key > begin_date:
             res.append((key, val))
-    return res
+    return [res, [begin_date + timedelta(days = 1), end_date + timedelta(days = 1)]]
           
 def getMonthData(data):
     end_date = data[-1][0] - timedelta(days = 7)
     begin_date = end_date - timedelta(days = 37)
-#    print end_date
-#    print begin_date
     res = []
     for (key, val) in data:
         if key <= end_date and key > begin_date:
             res.append((key, val))
-    return res
+    return [res, [begin_date + timedelta(days = 1), end_date + timedelta(days = 1)]]
     
 USER_ID = "a6ad00ac113a19d953efb91820d878"
 #DELTA = 300 # 5 mins
@@ -230,33 +233,32 @@ users_list = getUsersList()
 deltaDF = pandas.DataFrame()
 
 i = 0
+
 for single_user in users_list:
     i = i + 1
     user_name = "user" + str(i)
-    
-    #single_user = USER_ID
+
+#single_user = USER_ID
 #    hours_data = getHourData(single_user)
-#    with open("Quality/" + user_name + "_hour_data.csv", 'wb') as f:
+#    with open(user_name + "_hour_data.csv", 'wb') as f:
 #        writer = csv.writer(f)
 #        writer.writerows(hours_data)
-#        
-#    qualityHour = getQualityHour(single_user)
-#    with open("Quality/" + user_name + "_qualityHour.csv", 'wb') as f:
+    qualityHour = getQualityHour(single_user, 1)
+#    with open(user_name + "_qualityHour.csv", 'wb') as f:
 #        writer = csv.writer(f)
 #        writer.writerows(qualityHour)    
     
-    qualityDay = getQualityDay(single_user)
-    #print qualityDay[:100]
-    with open("Quality1/" + user_name + "_qualityDay.csv", 'wb') as f:
-        writer = csv.writer(f)
-        writer.writerows(qualityDay.items())
+#qualityDay = getQualityDay(single_user, 1)
     
-    #monthData = getMonthData(qualityDay)
-    #weekData = getWeekData(qualityDay)
+    #with open(single_user + "_qualityDay.csv", 'wb') as f:
+    #    writer = csv.writer(f)
+    #    writer.writerows(qualityDay)
     
-    plotAllData(qualityDay, user_name)
-    #plotMonthData(monthData, user_name)
-    #plotWeekData(weekData, user_name)
+    #plotAllData(qualityDay, user_name)
+    #plotMonthData(qualityDay, user_name)
+    #plotWeekData(qualityDay, user_name)
+    
+    plotRandomDays(qualityHour, user_name)
     
 #   single_user_data = getUserData(single_user)
 #   single_user_timestamp = getTimestamp(single_user_data) 
@@ -266,9 +268,7 @@ for single_user in users_list:
 #   #deltaDF.to_csv(single_user + ".csv")
 #   with open(single_user + ".csv", 'wb') as f:
 #       writer = csv.writer(f)
-#       writer.writerows(delta_list2)
-   
-    
+#       writer.writerows(delta_list2)  
     
     
     
