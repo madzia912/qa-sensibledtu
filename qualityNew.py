6f6ac89dbebcf4720389d3e4f4744c2a37a1f2b8
madzia912@gmail.com <madzia912@gmail.com@ccf55bd4-9b74-be2f-1e61-002a55cdb968>
2014-03-16 14:32:45 +0000

    Changed to save data in database

--- qualityNew.py
+++ qualityNew.py
@@ -5,19 +5,8 @@ Created on Wed Feb 12 14:55:00 2014
 @author: Magdalena Furman s110848
 """
 
-from datetime import datetime
 from datetime import timedelta
-from matplotlib import pyplot as plt
-from matplotlib.dates import date2num
-from matplotlib.dates import DateFormatter
-import pylab as pl
 import MySQLdb
-import csv
-from random import randrange
-
-# Diagram layout settings
-DATE_FMT = "%d-%m-%y"
-YLIM = [0, 1.1]
 
 # Quality settings
 HOUR_EXP = 12.0
@@ -29,8 +18,8 @@ USER = 'magda'
 PWD = 'lokus1?'
 HOST = 'localhost'
 
-DB_DATA = 'edu_mit_media_funf_probe_builtin_BluetoothProbe'
-#DB_DATA = 'edu_mit_media_funf_probe_builtin_LocationProbe'
+#DB_DATA = 'edu_mit_media_funf_probe_builtin_BluetoothProbe'
+DB_DATA = 'edu_mit_media_funf_probe_builtin_LocationProbe'
 #DB_DATA = 'edu_mit_media_funf_probe_builtin_WifiProbe'
 
 TB_QUALITY_DAILY = 'data_quality_daily'
@@ -39,13 +28,13 @@ TB_LAST_SCANS = 'user_last_scans'
 
 DB_QUALITY = 'data_quality'
 
-# TODO: move diagrams to another file
+# TODO: move drawing diagrams to another file
 # TODO: make the code more optimized in names and run it for WiFi and Location
 # see http://stackoverflow.com/questions/17779005/python-stringbuilder-for-mysql-query-execution
 
 def getUserLastScanId(db, user_id, column_name):
     cur = db.cursor()
-    cur.execute('SELECT bluetooth_id FROM user_last_scans WHERE user=%s', (user_id))
+    cur.execute('SELECT gps_id FROM user_last_scans WHERE user=%s', (user_id))
     result = cur.fetchall()
     cur.close()
     return result[0][0]
@@ -92,21 +81,9 @@ def getQualityDay(db, user_id, first_timestamp, last_timestamp, delta):
         first_timestamp = first_timestamp + timedelta(days = delta)
     return quality
     
-def getAllDataTimestamps(db, user_id):
-    cur = db.cursor()
-    cur.execute('SELECT DISTINCT(timestamp) FROM researcher WHERE user = %s ORDER BY timestamp', (user_id))
-    fetched_data = cur.fetchall()
-    last_timestamp = fetched_data[-1][0]
-    print last_timestamp
-    first_timestamp = fetched_data[0][0]
-    print first_timestamp
-    cur.close()
-    last_timestamp = datetime(last_timestamp.year, last_timestamp.month, last_timestamp.day, 0, 0, 0)
-    first_timestamp = datetime(first_timestamp.year, first_timestamp.month, first_timestamp.day, 0, 0, 0)
-    return [first_timestamp, last_timestamp]
-    
 def updateQualities(db, db_qual, user_id):
-    bluetooth_id = getUserLastScanId(db_qual, user_id, 'bluetooth_id')
+    bluetooth_id = getUserLastScanId(db_qual, user_id, 'location_id')
+    print 'BLUETOOTH: ', bluetooth_id
     cur = db.cursor()
     cur.execute('SELECT DISTINCT(timestamp) FROM researcher WHERE user = %s and id > %s ORDER BY timestamp', (user_id, bluetooth_id))
     fetched_data = cur.fetchall()
@@ -114,7 +91,17 @@ def updateQualities(db, db_qual, user_id):
     print len(fetched_data)
     if len(fetched_data) > 0:
         calcDayQuality(db_qual, fetched_data, user_id)
-    
+        cur = db.cursor()
+        cur.execute('SELECT id FROM researcher WHERE user = %s and id > %s ORDER BY id', (user_id, bluetooth_id))
+        fetched_data = cur.fetchall()
+        cur.close()
+        fetched_data
+        print fetched_data[-1][0]
+        cur = db_qual.cursor()
+        cur.execute('UPDATE user_last_scans SET gps_id = %s WHERE user = %s', (fetched_data[-1][0], user_id))
+        db_qual.commit()
+        cur.close()
+
 def calcDayQuality(db_qual, data, user_id):
     # TODO: change timestamps to 00:00:00 time
     last_timestamp = data[-1][0]
@@ -131,112 +118,24 @@ def calcDayQuality(db_qual, data, user_id):
         grade = count / DAY_EXP
         # TODO: make one big query
         cur = db_qual.cursor()        
+        #cur.execute('INSERT INTO data_quality_daily \
+        #(user, start_timestamp, end_timestamp, bluetooth_quality, \
+        #wifi_quality, location_quality, bluetooth_count, wifi_count, \
+        #location_count) VALUES (%s, %s, %s, %s, 0, 0, %s, 0, 0) \
+        #ON DUPLICATE KEY UPDATE bluetooth_quality = %s, bluetooth_count = %s', \
+        #(user_id, first_timestamp, end_timestamp, grade, count, grade, count))
+        
         cur.execute('INSERT INTO data_quality_daily \
         (user, start_timestamp, end_timestamp, bluetooth_quality, \
         wifi_quality, location_quality, bluetooth_count, wifi_count, \
-        location_count) VALUES (%s, %s, %s, %s, 0, 0, %s, 0, 0) \
-        ON DUPLICATE KEY UPDATE bluetooth_quality = %s, bluetooth_count = %s', \
+        location_count) VALUES (%s, %s, %s, 0, %s, 0, 0, %s, 0) \
+        ON DUPLICATE KEY UPDATE location_quality = %s, location_count = %s', \
         (user_id, first_timestamp, end_timestamp, grade, count, grade, count))
         
         db_qual.commit()
         cur.close()
         print "Key: " + str(first_timestamp) + "Grade:" + str(grade)
         first_timestamp = end_timestamp
-    
-def getLastWeekTimestamps(db, user_id):
-    cur = db.cursor()
-    cur.execute('SELECT timestamp FROM researcher WHERE user = %s ORDER BY timestamp DESC', (user_id))
-    last_timestamp = cur.fetchall()[0][0]
-    cur.close()
-    last_timestamp = datetime(last_timestamp.year, last_timestamp.month, last_timestamp.day, 0, 0, 0)
-    first_timestamp = last_timestamp - timedelta(days = 7)
-    return [first_timestamp, last_timestamp]
-   
-def getLastMonthTimestamps(db, user_id):
-    cur = db.cursor()
-    cur.execute('SELECT timestamp FROM researcher WHERE user = %s ORDER BY timestamp DESC', (user_id))
-    last_timestamp = cur.fetchall()[0][0]
-    cur.close()
-    last_timestamp = datetime(last_timestamp.year, last_timestamp.month, last_timestamp.day, 0, 0, 0)
-    last_timestamp = last_timestamp - timedelta(days = 7)
-    first_timestamp = last_timestamp - timedelta(days = 30)
-    return [first_timestamp, last_timestamp]
-    
-def commonPlotting(user_data, user_id, prefix, xlim=None):
-    x = [date2num(row[0]) for row in user_data]
-    y = [row[2] for row in user_data]
-    colors = getBarColour(y)
-    pl.figure()
-    pl.subplot(111)
-    pl.bar(x,y,color=colors)
-    pl.title('Quality of data for ' + user_id)
-    pl.xticks(rotation=30)
-    ax = pl.gca()
-    ax.xaxis.set_major_formatter(DateFormatter(DATE_FMT))
-    pl.xlim(xlim)
-    plt.show()
-    plt.savefig('pictures/' + prefix + user_id + '.png', bbox_inches='tight')
-    plt.close()
-    
-def getBarColour(data):
-    colors = []
-    for value in data:
-        if value >= 1.0:
-            colors.append('g')
-        else:
-            colors.append('r')
-    return colors
-    
-def plotAllData(user_data, user_id):
-    commonPlotting(user_data, user_id, 'all_')
-    
-def plotMonthData(user_data, user_id):
-    commonPlotting(user_data, user_id, 'month_')
-    
-def plotWeekData(user_data, user_id):
-    commonPlotting(user_data, user_id, 'week_')
-    
-def random_date(start, end):
-    """
-    This function will return a random datetime between two datetime 
-    objects.
-    """
-    delta = end - start
-    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
-    random_second = randrange(int_delta)
-    return start + timedelta(seconds=random_second)
-    
-def plotRandomDays(db, user_id, user_name):
-    cur = db.cursor()
-    cur.execute('SELECT timestamp FROM researcher WHERE user = %s ORDER BY timestamp DESC', (user_id))
-    fetched_data = cur.fetchall()   
-    #print fetched_data
-    last_timestamp = fetched_data[0][0]
-    first_timestamp = fetched_data[-1][0]
-    cur.close()
-    
-    for i in xrange(0, 3):
-        date = random_date(first_timestamp, last_timestamp)
-        start_date = datetime(date.year, date.month, date.day, 0, 0)
-        end_date = datetime(date.year, date.month, date.day, 23, 59, 59)
-        
-        quality = getQualityHour(db, user_id, start_date, end_date, 1)
-        x = [date2num(row[0]) for row in quality]
-        y = [row[2] for row in quality]
-        
-        colors = getBarColour(fetched_data)
-        pl.figure()
-        pl.subplot(111)
-        pl.bar(x,y,width=0.035, color=colors)
-        pl.xlim([start_date, end_date])
-        pl.title('Quality of data per hour, ' + user_name + " " + date.strftime(DATE_FMT))
-        pl.xticks(rotation=30)
-        #pl.ylim([0, 1.1])
-        ax = pl.gca()
-        ax.xaxis.set_major_formatter(DateFormatter("%H:%M")) 
-        plt.show()
-        plt.savefig('pictures/' + 'day_' + user_name + '_' + str(i) + '.png', bbox_inches='tight')
-        plt.close()
 
 db_data = MySQLdb.connect(host = HOST, user = USER, passwd = PWD, db = DB_DATA)
 db_quality = MySQLdb.connect(host = HOST, user = USER, passwd = PWD, db = DB_QUALITY)
